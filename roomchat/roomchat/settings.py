2c8fde4e97c960189e5329b7b41a0fad5244b4d6
MDGcode <adrianboscanici@yahoo.com>
2023-12-31 17:18:37 +0200

    fixed .env

--- roomchat/roomchat/settings.py
+++ roomchat/roomchat/settings.py
@@ -9,8 +9,9 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+from decouple import config
 from pathlib import Path
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qm8b#_x3-m80_+@&hq2gakxswpr7-ub(5xao=e=awsv0zv5+w-'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
